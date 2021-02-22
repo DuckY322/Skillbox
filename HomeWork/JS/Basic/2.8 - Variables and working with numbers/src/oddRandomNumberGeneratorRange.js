7f0b3cfb31924b87a446687a74b62a4f9aee5703
DuckY322 <ducky.show@mail.ru>
2021-02-22 20:04:36 +0700

    update work 8.5 weblayout

--- HomeWork/JS/Basic/2.8 - Variables and working with numbers/src/oddRandomNumberGeneratorRange.js
+++ HomeWork/JS/Basic/2.8 - Variables and working with numbers/src/oddRandomNumberGeneratorRange.js
@@ -1,16 +1,13 @@
-let numberN = 2;
-let numberM = 5;
+let numberN = -2;
+let numberM = -9;
 
 let min = Math.min(numberN, numberM);
 let max = Math.max(numberN, numberM);
 
-if (min % 2 === 0) {
-    min++;
-}
 
 let maxmin = max - min;
 
-let result = Math.floor(Math.random() * (maxmin / 2 + 1)) * 2 + min
+let result = Math.floor((Math.random() * maxmin + min) / 2) * 2 + 1;
 
 console.log("\n" + result);
 console.log("Сравнение чисел: " + Math.min(numberN, numberM) + " < " + Math.max(numberN, numberM));
