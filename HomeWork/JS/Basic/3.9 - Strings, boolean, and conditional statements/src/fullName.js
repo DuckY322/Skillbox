a0981d9c36168b29432481e9c3df59258e385f56
DuckY322 <ducky.show@mail.ru>
2021-03-07 12:52:06 +0700

    3th update JS HomeWork 3.9

--- HomeWork/JS/Basic/3.9 - Strings, boolean, and conditional statements/src/fullName.js
+++ HomeWork/JS/Basic/3.9 - Strings, boolean, and conditional statements/src/fullName.js
@@ -7,16 +7,23 @@ let firstSymbolNewSurname = "";
 let bodyNewName = "";
 let bodyNewSurname = "";
 
+let newName = "";
+let newSurname = "";
+
 firstSymbolNewName = oldName.substr(0, 1).toUpperCase();
 bodyNewName = oldName.substr(1).toLowerCase();
 firstSymbolNewSurname = oldSurname.substr(0, 1).toUpperCase();
 bodyNewSurname = oldSurname.substr(1).toLowerCase();
 
-console.log(`${firstSymbolNewName}${bodyNewName} ${firstSymbolNewSurname}${bodyNewSurname}`);
+newName = firstSymbolNewName + bodyNewName;
+newSurname = firstSymbolNewSurname + bodyNewSurname;
 
-const result =
-  oldName !== firstSymbolNewName+bodyNewName
-    ? console.log(`Имя было преобразовано`)
-    : oldSurname !== firstSymbolNewSurname+bodyNewSurname
-    ? console.log(`Имя было преобразовано`)
-    : console.log("Имя осталось без изменений");
+console.log(`${newName} ${newSurname}`);
+
+oldName !== newName
+  ? console.log(`Имя было преобразовано`)
+  : console.log("Имя осталось без изменений");
+
+oldSurname !== newSurname
+  ? console.log(`Фамилия была преобразована`)
+  : console.log("Фамилия осталась без изменений");
