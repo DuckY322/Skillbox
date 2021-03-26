c3ac1b197a9616935e9102e762e0c34648d505ec
DuckY322 <ducky.show@mail.ru>
2021-03-26 19:11:44 +0700

    9th update Weblayout homework 13.8, done: added animations, fixed burger-menu

--- HomeWork/Weblayout/Basic/13.8 - Javascript for the web layout designer/js/index.js
+++ HomeWork/Weblayout/Basic/13.8 - Javascript for the web layout designer/js/index.js
@@ -14,23 +14,74 @@ const swiper = new Swiper('.swiper-container', {
 
 document.addEventListener('DOMContentLoaded', function() {
 
+    showElement('.zero-block', '.zero-block__title', null, 'active-element');
+    showElement('.zero-block', '.zero-block__subTitle', null, 'active-element-reverse');
+    showElement('.zero-block', '.zero-block__building-image', null, 'active-child-element');
+    showElement('.zero-block', '.zero-block__building-image', 0, 'active-child-element-up-1');
+    showElement('.zero-block', '.zero-block__building-image', 1, 'active-child-element-up-2');
+    showElement('.zero-block', '.zero-block__building-image', 2, 'active-child-element-up-3');
+    showElement('.zero-block', '.zero-block__building-image', 3, 'active-child-element-up-4');
+    showElement('.zero-block', '.zero-block__building-image', 4, 'active-child-element-up-5');
+
     let openQuestion;
 
-    function viewHideMenu() {
+    function showHideMenu() {
         document.querySelector('.header__list').classList.toggle('header__list_active');
-        document.body.classList.toggle('burger-menu-noScroll');
         document.querySelector('.header__button_burger').classList.toggle('header__button_burger_active');
     }
 
+    function showElement(position, element, number, effect) {
+        if (number === null) {
+            document.querySelector(position).querySelectorAll(element).forEach(el => {
+                if (!el.classList.contains(effect)) {
+                    var br = el.getBoundingClientRect();
+                    let brTop = Math.round(br.top - window.screen.height);
+
+                    if (brTop < 0) {
+                        el.classList.add(effect);
+                    }
+                } else {
+                    return;
+                }
+            });
+        } else {
+            let item = document.querySelector(position).querySelectorAll(element)[number];
+            if (!item.classList.contains(effect)) {
+                var br = item.getBoundingClientRect();
+                let brTop = Math.round(br.top - window.screen.height);
+
+                if (brTop < 0) {
+                    item.classList.add(effect);
+                }
+            } else {
+                return;
+            }
+        };
+    }
+
+    window.addEventListener('scroll', function() {
+        showElement('main', '.section-design-solutions', null, 'active-element-up');
+        showElement('main', '.section-about-us', null, 'active-element-reverse');
+        showElement('main', '.section-how-we-work', null, 'active-element');
+        showElement('main', '.section-questions', null, 'active-element-down');
+        showElement('.section-questions', '.section-questions__item', null, 'active-child-element');
+        showElement('.section-questions', '.section-questions__item', 0, 'active-child-element-1');
+        showElement('.section-questions', '.section-questions__item', 1, 'active-child-element-2');
+        showElement('.section-questions', '.section-questions__item', 2, 'active-child-element-3');
+        showElement('.section-questions', '.section-questions__item', 3, 'active-child-element-4');
+        showElement('.section-questions', '.section-questions__item', 4, 'active-child-element-5');
+    });
+
     document.querySelector('.header__button_burger').addEventListener('click', function() {
-        viewHideMenu();
+        showHideMenu();
     })
 
     document.querySelectorAll('.header__link').forEach(el => {
         el.addEventListener('click', function(e) {
-            if (document.body.classList.contains('burger-menu-noScroll')) {
-                viewHideMenu();
+            if (document.querySelector('.header__list').classList.contains('header__list_active')) {
+                showHideMenu();
             }
+
             e.preventDefault();
             let scrollto = this.getAttribute('scrollto');
             const scrollTarget = document.querySelector(scrollto);
