d081de843225c75728bd65172aaf0892754f9d4a
DuckY322 <ducky.show@mail.ru>
2021-04-07 20:24:55 +0700

    initial JS HomeWork 9.7; done: created first page - menu

---
+++ HomeWork/JS/Basic/9.7 - Constants, scopes, and closures/src/index.js
@@ -0,0 +1,39 @@
+document.addEventListener(`DOMContentLoaded`, function () {
+
+    const menu = document.querySelector(`.section-menu`);
+    const NickName = document.getElementById(`input-nickName`);
+    const cardCountVertical = document.getElementById(`input-cardCountVert`);
+    const cardCountHorizont = document.getElementById(`input-cardCountHor`);
+    const btnStartGame = document.querySelector(`.section-menu__btn`);
+
+    function startGame() {
+        if (NickName.value) {
+            if ((parseInt(cardCountVertical.value) && parseInt(cardCountVertical.value) % 2 === 0 && parseInt(cardCountVertical.value) >= 2 && parseInt(cardCountVertical.value) <= 10) &&
+                (parseInt(cardCountHorizont.value) && parseInt(cardCountHorizont.value) % 2 === 0 && parseInt(cardCountHorizont.value) >= 2 && parseInt(cardCountHorizont.value) <= 10)) {
+                menu.classList.toggle(`off`);
+            } else {
+                alert(`Укажите корректное количество карточек`);
+            }
+        } else {
+            alert(`Введите, пожалуйста, никнейм`);
+        }
+    };
+
+    btnStartGame.addEventListener(`click`, function () {
+        startGame();
+    });
+
+    NickName.addEventListener(`change`, function () {
+        startGame();
+    });
+
+    cardCountVertical.addEventListener(`change`, function () {
+        startGame();
+    });
+
+    cardCountHorizont.addEventListener(`change`, function () {
+        startGame();
+    });
+
+
+});
