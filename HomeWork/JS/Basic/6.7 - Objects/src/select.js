ba958d8e1e76de7d2f1bbd2b37cb1c575e0bbd57
DuckY322 <ducky.show@mail.ru>
2021-04-08 23:11:40 +0700

    7nd update JS HomeWork 6.7; done: optimization and renaming code in select.js

--- HomeWork/JS/Basic/6.7 - Objects/src/select.js
+++ HomeWork/JS/Basic/6.7 - Objects/src/select.js
@@ -1,17 +1,16 @@
-function convert(arrConvert) {
+function arrConverting(sourceArr) {
   let readyArr = [];
 
-  for (const item of arrConvert) {
+  for (const item of sourceArr) {
     if (typeof item === "object") {
       if (item.value && item.label) {
-        readyArr.push(item);
+        readyArr.push({ value: item.value, label: item.label });
       } else if (!item.value && item.label) {
         readyArr.push({ value: item.label, label: item.label });
       } else if (item.value && !item.label) {
         readyArr.push({ value: item.value, label: item.value });
       } else {
         const entries = Object.entries(item);
-
         for (const [key, value] of entries) {
           readyArr.push({ value: value, label: value });
         }
@@ -24,27 +23,18 @@ function convert(arrConvert) {
   return readyArr;
 }
 
-function createSelect(arrOpt, valueOpt) {
-  const readyArr = convert(arrOpt);
+function creatingSelect(optionsArr, selectedOption) {
+  const readyArr = arrConverting(optionsArr);
   const select = document.createElement(`select`);
   const body = document.body;
 
-  for (const obj of readyArr) {
+  for (const item of readyArr) {
     const option = document.createElement(`option`);
-    const entries = Object.entries(obj);
+    
+    option.value = item.value;
+    option.textContent = item.label;
 
-    for (const [key, value] of entries) {
-      if (key === `value`) {
-        option.value = value;
-        continue;
-      }
-      if (key === `label`) {
-        option.textContent = value;
-        continue;
-      }
-    }
-
-    if (option.value === valueOpt) {
+    if (option.value === selectedOption) {
       option.selected = true;
     }
 
@@ -55,18 +45,3 @@ function createSelect(arrOpt, valueOpt) {
 
   return select;
 }
-
-createSelect(
-  [
-    { value: `Москва`, label: `Москва` },
-    { value: `Санкт-Петергбург`, label: `Санкт-Петергбург` },
-    { value: `Ростов`, label: `Ростов` },
-    { value: `Новосибирск`, label: `Новосибирск` },
-    { value: `Иркутск`, label: `Иркутск` },
-    { value: `Казань`, label: `Казань` },
-    { value1: `value11`, label1: `label11` },
-    1,
-    `Ура`,
-  ],
-  `Новосибирск`
-);
