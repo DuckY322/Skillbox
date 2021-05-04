7531819607dc20c30464e20be8cf4c2dd739176a
DuckY322 <ducky.show@mail.ru>
2021-05-04 14:00:22 +0700

    3nd update JS HomeWork 10.6; done: complete

--- HomeWork/JS/Basic/10.6 - Type conversion and non-strict comparisons/src/index.js
+++ HomeWork/JS/Basic/10.6 - Type conversion and non-strict comparisons/src/index.js
@@ -1,6 +1,20 @@
-let myStorage = window.localStorage;
-let students = [];
-let filteredBy;
+const myStorage = window.localStorage;
+const dateFromStorage = JSON.parse(myStorage.getItem('table'));
+const students = dateFromStorage !== null ? dateFromStorage : [];
+const filteredBy = [
+    {
+        FullName: null,
+    },
+    {
+        Faculty: null,
+    },
+    {
+        dateOfBirth: null,
+    },
+    {
+        YearOfBeginningOfTraining: null,
+    },
+];
 
 function calculatingTime(date) {
     return ((new Date().getTime() - new Date(date)) / (24 * 3600 * 365.25 * 1000)) | 0;
@@ -12,7 +26,7 @@ function createAppTitle(title) {
     return appTile;
 };
 
-function createSortMenu(handlers) {
+function createSortMenu() {
     const dropDown = document.createElement(`div`);
     dropDown.classList.add(`dropdown`, `mb-3`);
 
@@ -29,134 +43,66 @@ function createSortMenu(handlers) {
     dropdownMenu.classList.add(`dropdown-menu`);
     dropdownMenu.setAttribute(`aria-labelledby`, `dropdownMenuButton`);
 
-    const SortByName = document.createElement(`a`);
-    SortByName.classList.add(`dropdown-item`);
-    SortByName.href = `#`;
-    SortByName.textContent = `По имени`;
-
-    const SortByFaculty = document.createElement(`a`);
-    SortByFaculty.classList.add(`dropdown-item`);
-    SortByFaculty.href = `#`;
-    SortByFaculty.textContent = `По факультету`;
-
-    const SortByAge = document.createElement(`a`);
-    SortByAge.classList.add(`dropdown-item`);
-    SortByAge.href = `#`;
-    SortByAge.textContent = `По возрасту`;
-
-    const SortByYearOfBeg = document.createElement(`a`);
-    SortByYearOfBeg.classList.add(`dropdown-item`);
-    SortByYearOfBeg.href = `#`;
-    SortByYearOfBeg.textContent = `По году начала обучения`;
-
-    SortByName.addEventListener(`click`, handlers.sortByName)
-    SortByFaculty.addEventListener(`click`, handlers.sortByFaculty)
-    SortByAge.addEventListener(`click`, handlers.sortByAge)
-    SortByYearOfBeg.addEventListener(`click`, handlers.sortByYearOfBeg)
-
-    dropdownMenu.append(SortByName);
-    dropdownMenu.append(SortByFaculty);
-    dropdownMenu.append(SortByAge);
-    dropdownMenu.append(SortByYearOfBeg);
-
     dropDown.append(btnDropDownToggle);
     dropDown.append(dropdownMenu);
 
-    return dropDown;
+    return {
+        dropDown,
+        dropdownMenu
+    };
 }
 
-function createFilterMenu(handlers) {
+function createSortMenuItem(handlers, parameters) {
+    const item = document.createElement(`a`);
+    item.classList.add(`dropdown-item`);
+    item.href = `#`;
+    item.textContent = parameters.title;
+
+    item.addEventListener(`click`, handlers[parameters.sortBy]);
+
+    return item;
+}
+
+function createFilterMenu(handlers, parameters) {
+
     const inputGroup = document.createElement(`div`);
     inputGroup.classList.add(`input-group`, `mb-3`);
 
-    const inputPrepend = document.createElement(`div`);
-    inputPrepend.classList.add(`input-group-prepend`);
+    const inputGroupPrepend = document.createElement(`div`);
+    inputGroupPrepend.classList.add(`input-group-prepend`);
+
+    const inputGroupText = document.createElement(`span`);
+    inputGroupText.classList.add(`input-group-text`);
+    inputGroupText.id = `inputGroup-sizing-default`;
+    inputGroupText.textContent = parameters.title;
 
     const input = document.createElement(`input`);
     input.type = `text`;
     input.classList.add(`form-control`);
-    input.setAttribute(`aria-label`, `Text input with dropdown button`)
+    input.setAttribute(`aria-label`, `Sizing example input`);
+    input.setAttribute(`aria-describedby`, `inputGroup-sizing-default`);
     input.oninput = function () {
-        handlers.listFiltering(input.value, filteredBy);
+        handlers.listFiltering(input.value, parameters.value);
     }
 
-    const btnDropDownToggle = document.createElement(`button`);
-    btnDropDownToggle.classList.add(`btn`, `btn-outline-secondary`, `dropdown-toggle`);
-    btnDropDownToggle.type = `button`;
-    btnDropDownToggle.setAttribute(`data-toggle`, `dropdown`);
-    btnDropDownToggle.setAttribute(`aria-haspopup`, `true`);
-    btnDropDownToggle.setAttribute(`aria-expanded`, `false`);
-    btnDropDownToggle.textContent = `Фильтровать`;
+    inputGroupPrepend.append(inputGroupText);
 
-    const dropdownMenu = document.createElement(`div`);
-    dropdownMenu.classList.add(`dropdown-menu`);
-
-    const FilterByName = document.createElement(`a`);
-    FilterByName.classList.add(`dropdown-item`);
-    FilterByName.href = `#`;
-    FilterByName.textContent = `По имени`;
-
-    const FilterByFaculty = document.createElement(`a`);
-    FilterByFaculty.classList.add(`dropdown-item`);
-    FilterByFaculty.href = `#`;
-    FilterByFaculty.textContent = `По факультету`;
-
-    const FilterByAge = document.createElement(`a`);
-    FilterByAge.classList.add(`dropdown-item`);
-    FilterByAge.href = `#`;
-    FilterByAge.textContent = `По возрасту`;
-
-    const FilterByYearOfBeg = document.createElement(`a`);
-    FilterByYearOfBeg.classList.add(`dropdown-item`);
-    FilterByYearOfBeg.href = `#`;
-    FilterByYearOfBeg.textContent = `По году начала обучения`;
-
-    const separator = document.createElement(`div`);
-    separator.setAttribute(`role`, `separator`);
-    separator.classList.add(`dropdown-divider`);
-
-    FilterByName.addEventListener(`click`, function () { handlers.filterByName(FilterByName, btnDropDownToggle) })
-    FilterByFaculty.addEventListener(`click`, function () { handlers.filterByFaculty(FilterByFaculty, btnDropDownToggle) })
-    FilterByAge.addEventListener(`click`, function () { handlers.filterByAge(FilterByAge, btnDropDownToggle) })
-    FilterByYearOfBeg.addEventListener(`click`, function () { handlers.filterByYearOfBeg(FilterByYearOfBeg, btnDropDownToggle) })
-
-    dropdownMenu.append(FilterByName);
-    dropdownMenu.append(FilterByFaculty);
-    dropdownMenu.append(FilterByAge);
-    dropdownMenu.append(FilterByYearOfBeg);
-    dropdownMenu.append(separator);
-
-    inputPrepend.append(btnDropDownToggle);
-    inputPrepend.append(dropdownMenu);
-
-    inputGroup.append(inputPrepend);
+    inputGroup.append(inputGroupPrepend);
     inputGroup.append(input);
 
     return inputGroup;
 }
 
-function createSortList(oldList, parameter) {
-
-    const newStudentList = students.slice();
-
-    newStudentList.sort((prev, next) => {
-        if (typeof parameter === `object`) {
-            if (`${prev[parameter[0]]} ${prev[parameter[1]]} ${prev[parameter[2]]}` < `${next[parameter[0]]} ${next[parameter[1]]} ${next[parameter[2]]}`) return -1;
-            if (`${prev[parameter[0]]} ${prev[parameter[1]]} ${prev[parameter[2]]}` < `${next[parameter[0]]} ${next[parameter[1]]} ${next[parameter[2]]}`) return 1;
-        } else {
-            if (prev[parameter] < next[parameter]) return -1;
-            if (prev[parameter] < next[parameter]) return 1;
-        }
+function sortList(oldList, parameter) {
+    students.sort((prev, next) => {
+        if (prev[parameter] < next[parameter]) return -1;
+        if (prev[parameter] < next[parameter]) return 1;
     });
 
-    oldList.innerHTML = ``;
-    newStudentList.forEach(student => {
-        const studentElement = createStudent(student);
-        oldList.append(studentElement);
-    });
+    createNewStudentsList(oldList, students)
 }
 
-function createFilterList(oldList, newStudentList) {
+function createNewStudentsList(oldList, newStudentList) {
     oldList.innerHTML = ``;
     newStudentList.forEach(student => {
         const studentElement = createStudent(student);
@@ -171,7 +117,10 @@ function createStudentCreateForm() {
     const dateNowMonth = dateNow.getMonth();
     const dateNowDay = dateNow.getDate();
 
+    const inputs = [];
+
     const form = document.createElement(`form`);
+    form.classList.add(`input-group`, `mb-5`);
 
     const inputFirstName = document.createElement(`input`);
     const inputSurname = document.createElement(`input`);
@@ -204,27 +153,34 @@ function createStudentCreateForm() {
     inputFaculty.classList.add(`form-control`, `w-100`, `mb-1`);
     inputFaculty.placeholder = `Введите факультет студента`;
 
-    form.append(inputFirstName);
-    form.append(inputSurname);
-    form.append(inputMiddleName);
-    form.append(inputdateOfBirth);
-    form.append(inputYearOfBeginningOfTraining);
-    form.append(inputFaculty);
-
     const buttonWrapper = document.createElement(`div`);
-    const button = document.createElement(`button`);
-
-    form.classList.add(`input-group`, `mb-5`);
     buttonWrapper.classList.add(`input-group-append`, `mr-3`);
+
+    const button = document.createElement(`button`);
     button.setAttribute(`disabled`, `disabled`);
     button.classList.add(`btn`, `btn-primary`);
     button.textContent = `Добавить`;
 
+    form.append(inputFirstName);
+    inputs.push(inputFirstName);
+    form.append(inputSurname);
+    inputs.push(inputSurname);
+    form.append(inputMiddleName);
+    inputs.push(inputMiddleName);
+    form.append(inputdateOfBirth);
+    inputs.push(inputdateOfBirth);
+    form.append(inputYearOfBeginningOfTraining);
+    inputs.push(inputYearOfBeginningOfTraining);
+    form.append(inputFaculty);
+    inputs.push(inputFaculty);
+
     buttonWrapper.append(button);
+
     form.append(buttonWrapper);
 
     return {
         form,
+        inputs,
         inputFirstName,
         inputSurname,
         inputMiddleName,
@@ -267,7 +223,7 @@ function createStudent(studentInfo) {
     const age = calculatingTime(studentInfo.dateOfBirth);
     const course = calculatingTime(`${studentInfo.YearOfBeginningOfTraining}-09-01`) + 1;
 
-    studentName.textContent = `ФИО: ${studentInfo.Surname} ${studentInfo.FirstName} ${studentInfo.MiddleName}`;
+    studentName.textContent = `ФИО: ${studentInfo.FullName}`;
     studentdateOfBirth.textContent = `Дата рождения: ${dateOfBirth} ( Возраст: ${age} )`;
     studentYearOfBeginningOfTraining.textContent = `Годы обучения: ${studentInfo.YearOfBeginningOfTraining}-${Number(studentInfo.YearOfBeginningOfTraining) + 4} ${course <= 4 && course >= 1 ? ` ( ${course} курс )` : `( Закончил(а) )`}`;
     studentFaculty.textContent = `Факультет: ${studentInfo.Faculty}`;
@@ -284,93 +240,117 @@ function createStudentControlPanelApp(container) {
     const handlers = [
         {
             sortByName() {
-                createSortList(studentsList, [`Surname`, `FirstName`, `MiddleName`]);
+                sortList(studentsList, `FullName`);
             },
             sortByFaculty() {
-                createSortList(studentsList, `Faculty`);
+                sortList(studentsList, `Faculty`);
             },
             sortByAge() {
-                createSortList(studentsList, `dateOfBirth`);
+                sortList(studentsList, `dateOfBirth`);
             },
             sortByYearOfBeg() {
-                createSortList(studentsList, `YearOfBeginningOfTraining`);
+                sortList(studentsList, `YearOfBeginningOfTraining`);
             }
         },
         {
-            filterByName(thisBtn, mainBtn) {
-                filteredBy = [`Surname`, `FirstName`, `MiddleName`];
-                mainBtn.textContent = thisBtn.textContent;
-            },
-            filterByFaculty(thisBtn, mainBtn) {
-                filteredBy = `Faculty`;
-                mainBtn.textContent = thisBtn.textContent;
-            },
-            filterByAge(thisBtn, mainBtn) {
-                filteredBy = `dateOfBirth`;
-                mainBtn.textContent = thisBtn.textContent;
-            },
-            filterByYearOfBeg(thisBtn, mainBtn) {
-                filteredBy = `YearOfBeginningOfTraining`;
-                mainBtn.textContent = thisBtn.textContent;
-            },
-            listFiltering(text, filteredBy) {
-                if (filteredBy) {
-                    let FilterList;
-                    if (typeof filteredBy === `object`) {
-                        FilterList = students.filter(item => item[filteredBy[0]].includes(text) || item[filteredBy[1]].includes(text) || item[filteredBy[2]].includes(text));
-                    } else {
-                        FilterList = students.filter(item => item[filteredBy].includes(text));
-                    }
-
-                    createFilterList(studentsList, FilterList);
+            listFiltering(text, datafilteredBy) {
+                if (text !== '') {
+                    const index = filteredBy.findIndex(item => Object.keys(item)[0] === datafilteredBy);
+                    filteredBy[index][datafilteredBy] = text;
+                } else {
+                    const index = filteredBy.findIndex(item => Object.keys(item)[0] === datafilteredBy);
+                    filteredBy[index][datafilteredBy] = null;
                 }
+
+                let newStudentsList = students.filter(student => {
+                    let result = false;
+                    let filterParametersCount = 0;
+                    let numberOfFiltersPassed = 0;
+
+                    filteredBy.forEach(item => {
+                        const key = Object.keys(item)[0];
+                        if (item[key]) {
+                            filterParametersCount++;
+                        }
+                    });
+
+                    filteredBy.forEach(item => {
+                        const key = Object.keys(item)[0];
+                        if (item[key]) {
+                            if (student[key].includes(item[key])) {
+                                numberOfFiltersPassed++;
+                                if (filterParametersCount === numberOfFiltersPassed) {
+                                    result = true;
+                                    return;
+                                }
+                            }
+                        }
+                    });
+
+                    return result;
+                });
+
+                createNewStudentsList(studentsList, newStudentsList.length > 0 ? newStudentsList : students);
             }
         }
     ]
+    const filteredParameters = [
+        {
+            title: `По имени`,
+            value: `FullName`,
+            sortBy: `sortByName`,
+        },
+        {
+            title: `По факультету`,
+            value: `Faculty`,
+            sortBy: `sortByFaculty`,
+        },
+        {
+            title: `По году рождения`,
+            value: `dateOfBirth`,
+            sortBy: `sortByAge`,
+        },
+        {
+            title: `По году начала обучения`,
+            value: `YearOfBeginningOfTraining`,
+            sortBy: `sortByYearOfBeg`,
+        },
+    ];
     const appTitleAdd = createAppTitle(`Добавить студента`);
     const appTitleList = createAppTitle(`Список студентов`);
     const studentCreateForm = createStudentCreateForm();
-    const sortMenu = createSortMenu(handlers[0]);
-    const filterMenu = createFilterMenu(handlers[1]);
+    const sortMenu = createSortMenu();
     const studentsList = createStudentsList();
 
     container.append(appTitleAdd);
     container.append(studentCreateForm.form);
     container.append(appTitleList);
-    container.append(sortMenu);
-    container.append(filterMenu);
+
+    container.append(sortMenu.dropDown);
+
+    filteredParameters.forEach(el => {
+        sortMenu.dropdownMenu.append(createSortMenuItem(handlers[0], el));
+        container.append(createFilterMenu(handlers[1], el));
+    });
+
     container.append(studentsList);
 
-    dateFromStorage = JSON.parse(myStorage.getItem('table'));
-
-    students = dateFromStorage !== null ? dateFromStorage : [];
     students.forEach(student => {
         const studentElement = createStudent(student);
         studentsList.append(studentElement);
     });
 
-    let inputs = [
-        studentCreateForm.inputFirstName,
-        studentCreateForm.inputSurname,
-        studentCreateForm.inputMiddleName,
-        studentCreateForm.inputdateOfBirth,
-        studentCreateForm.inputYearOfBeginningOfTraining,
-        studentCreateForm.inputFaculty,
-    ]
-
-    inputs.forEach(item => {
+    studentCreateForm.inputs.forEach(item => {
         item.oninput = function () {
-            disabledFormButton(inputs, studentCreateForm.button);
+            disabledFormButton(studentCreateForm.inputs, studentCreateForm.button);
         }
     });
 
     studentCreateForm.form.addEventListener(`submit`, async e => {
         e.preventDefault();
 
-        let studentObj = {
-            FirstName: studentCreateForm.inputFirstName.value.trim(),
-            Surname: studentCreateForm.inputSurname.value.trim(),
-            MiddleName: studentCreateForm.inputMiddleName.value.trim(),
+        const studentObj = {
+            FullName: `${studentCreateForm.inputSurname.value.trim()} ${studentCreateForm.inputFirstName.value.trim()} ${studentCreateForm.inputMiddleName.value.trim()}`,
             dateOfBirth: studentCreateForm.inputdateOfBirth.value.trim(),
             YearOfBeginningOfTraining: studentCreateForm.inputYearOfBeginningOfTraining.value.trim(),
             Faculty: studentCreateForm.inputFaculty.value.trim(),
@@ -379,10 +359,11 @@ function createStudentControlPanelApp(container) {
         const studentElement = createStudent(studentObj);
         studentsList.append(studentElement);
 
-        students.push(studentObj);
-        myStorage.setItem('table', JSON.stringify(students))
+        const tempStorage = JSON.parse(myStorage.getItem('table'));
+        tempStorage.push(studentObj);
+        myStorage.setItem('table', JSON.stringify(tempStorage))
 
-        inputs.forEach(item => {
+        studentCreateForm.inputs.forEach(item => {
             item.value = ``;
         });
 
