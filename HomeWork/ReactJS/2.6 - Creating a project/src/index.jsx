e15e6f51404ff1055654c68df86989b800240e0e
Валерия Кузнецова <ducky.show@mail.ru>
2021-05-11 13:22:17 +0700

    initial ReactJS HomeWork 2.6; done: save local files

---
+++ HomeWork/ReactJS/2.6 - Creating a project/src/index.jsx
@@ -0,0 +1,6 @@
+import React from 'react';
+import ReactDOM from 'react-dom'
+
+window.addEventListener('load', () => {
+    ReactDOM.render(<header/>, document.getElementById(('react_root')));
+});
